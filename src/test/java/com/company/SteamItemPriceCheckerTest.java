23daabc692cb9d3ff4389d6bd0971d8181372d75
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2021-09-24 22:50:48 +0200

    added test

--- src/test/java/com/company/SteamItemPriceCheckerTest.java
+++ src/test/java/com/company/SteamItemPriceCheckerTest.java
@@ -6,8 +6,13 @@ import static com.company.SteamItemPriceChecker.getSteamPriceForGivenName;
 
 public class SteamItemPriceCheckerTest extends TestCase {
 
-    public void testGetSteamPriceForGivenName() throws Exception {
+    public void testGetSteamPriceForGivenNamePositive() throws Exception {
         String itemName = "UMP-45 | Fade (Factory New)";
         assertTrue(getSteamPriceForGivenName(itemName)>0.0);
+    }
+
+    public void testGetSteamPriceForGivenNameNegative() throws Exception {
+        String itemName = "Test Item Never will be found";
+        assertTrue(getSteamPriceForGivenName(itemName)==0.0);
     }
 }
