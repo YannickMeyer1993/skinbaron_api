1acd5baac765572a11fd7f43fa746d6b3eb2af4a
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2021-10-10 13:19:42 +0200

    added Buff crawler

--- src/main/java/com/company/BuffCrawler.java
+++ src/main/java/com/company/BuffCrawler.java
@@ -2,11 +2,14 @@ package com.company;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.*;
-import org.dom4j.*;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
-import org.dom4j.tree.AbstractNode;
 
+import javax.swing.text.html.StyleSheet;
 import java.io.IOException;
 import java.io.StringReader;
 import java.math.RoundingMode;
@@ -40,27 +43,6 @@ public class BuffCrawler {
         conn.setAutoCommit(false);
         System.out.println("Successfully Connected.");
 
-        String SQLUpsert = "WITH\n" +
-                "    to_be_upserted (id,price_euro,timestamp,success,name) AS (\n" +
-                "        VALUES\n" +
-                "            (?,?,?,?,?,?)\n" +
-                "    ),\n" +
-                "    updated AS (\n" +
-                "        UPDATE\n" +
-                "            steam_item_sale.buff_item_prices s\n" +
-                "        SET\n" +
-                "            price = to_be_upserted.price::numeric\n" +
-                "            ,timestamp = to_be_upserted.price::numeric\n" +
-                "        FROM\n" +
-                "            to_be_upserted\n" +
-                "        WHERE\n" +
-                "            s.id = to_be_upserted.id\n" +
-                "        RETURNING s.id\n" +
-                "    )\n" +
-                "INSERT INTO steam_item_sale.buff_item_prices\n" +
-                "    SELECT * FROM to_be_upserted\n" +
-                "    WHERE id NOT IN (SELECT id FROM updated);";
-
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery("select id from (select\n" +
                 "\trank() over ( partition by weapon_name order by timestamp ) as ranking,\n" +
@@ -78,8 +60,12 @@ public class BuffCrawler {
                 "order by id");
 
         while (rs.next()) {
-            getBuffItem(conn, 38586);//rs.getInt("id"));
-            break;
+            try {
+                getBuffItem(conn, rs.getInt("id"));
+            } catch (IndexOutOfBoundsException e)
+            {
+                getBuffItemNoExterior(conn, rs.getInt("id"));
+            }
         }
 
         rs.close();
@@ -88,12 +74,120 @@ public class BuffCrawler {
 
     }
 
-    private static void getBuffItem(Connection conn, int id) throws IOException, InterruptedException, DocumentException {
+    private static void getBuffItemNoExterior(Connection conn, int id) throws SQLException, InterruptedException, IOException, DocumentException {
+
+        String SQLUpsert = "WITH\n" +
+                "    to_be_upserted (id,price_euro,timestamp,has_enterior,name) AS (\n" +
+                "        VALUES\n" +
+                "            (?,?,current_timestamp,?,?)\n" +
+                "    ),\n" +
+                "    updated AS (\n" +
+                "        UPDATE\n" +
+                "            steam_item_sale.buff_item_prices s\n" +
+                "        SET\n" +
+                "            price_EURO = to_be_upserted.price_EURO::numeric\n" +
+                "            ,timestamp = to_be_upserted.timestamp\n" +
+                "        FROM\n" +
+                "            to_be_upserted\n" +
+                "        WHERE\n" +
+                "            s.id = to_be_upserted.id\n" +
+                "        RETURNING s.id\n" +
+                "    )\n" +
+                "INSERT INTO steam_item_sale.buff_item_prices\n" +
+                "    SELECT * FROM to_be_upserted\n" +
+                "    WHERE id NOT IN (SELECT id FROM updated);";
 
         DecimalFormat df = new DecimalFormat("0.00");
         df.setRoundingMode(RoundingMode.HALF_UP);
 
-        List<String[]> result = new ArrayList<String[]>();
+        List<String[]> result = new ArrayList<>();
+
+        String url = "https://buff.163.com/market/goods?goods_id=" + id;
+
+        WebClient webClient = new WebClient(BrowserVersion.FIREFOX);
+        webClient.getOptions().setJavaScriptEnabled(true); // enable javascript
+        webClient.getOptions().setCssEnabled(true);
+        webClient.getOptions().setThrowExceptionOnScriptError(false); //even if there is error in js continue
+        webClient.waitForBackgroundJavaScriptStartingBefore(1000000);
+        webClient.waitForBackgroundJavaScript(10000000); // important! wait when javascript finishes rendering
+        HtmlPage page = webClient.getPage(url);
+        Thread.sleep(4000); //needed to load page
+
+        String hash_name = null;
+        List<DomElement> names = page.getByXPath("//*[contains(@class, 'cru-goods')]");
+        for (DomElement name : names) {
+            Document name_xml = new SAXReader().read(new StringReader(name.asXml()));
+            if (name_xml.valueOf("span") == null) {
+                continue;
+            }
+            hash_name = name_xml.valueOf("span").trim();
+        }
+
+        System.out.println(hash_name);
+
+        List<com.gargoylesoftware.htmlunit.html.DomElement> Items = page.getByXPath("//*[contains(@class, 'f_Strong')]");
+
+        try (PreparedStatement pstmt = conn.prepareStatement(SQLUpsert, Statement.RETURN_GENERATED_KEYS)) {
+
+            double min_price_rmb = Double.MAX_VALUE;
+            for (DomElement elem: Items){
+
+                try {
+                    double price_rmb = Double.parseDouble(elem.asNormalizedText().replace("¥ ","").trim());
+                    if (min_price_rmb > price_rmb){
+                        min_price_rmb = price_rmb;
+                    }
+                } catch (NumberFormatException e){
+                    //do nothing
+                }
+
+            }
+
+            if (min_price_rmb == Double.MAX_VALUE){ return;}
+
+            double price_euro = Double.parseDouble(df.format(conversionFromRMBtoEUR * min_price_rmb).replace(",","."));
+
+            pstmt.setInt(1, id);
+            pstmt.setDouble(2, price_euro);
+            pstmt.setBoolean(3, false);
+            pstmt.setString(4, hash_name);
+
+            pstmt.execute();
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        conn.commit();
+    }
+
+    private static void getBuffItem(Connection conn, int id) throws IOException, InterruptedException, DocumentException, SQLException, IndexOutOfBoundsException {
+
+        String SQLUpsert = "WITH\n" +
+                "    to_be_upserted (id,price_euro,timestamp,has_enterior,name) AS (\n" +
+                "        VALUES\n" +
+                "            (?,?,current_timestamp,?,?)\n" +
+                "    ),\n" +
+                "    updated AS (\n" +
+                "        UPDATE\n" +
+                "            steam_item_sale.buff_item_prices s\n" +
+                "        SET\n" +
+                "            price_EURO = to_be_upserted.price_EURO::numeric\n" +
+                "            ,timestamp = to_be_upserted.timestamp\n" +
+                "        FROM\n" +
+                "            to_be_upserted\n" +
+                "        WHERE\n" +
+                "            s.id = to_be_upserted.id\n" +
+                "        RETURNING s.id\n" +
+                "    )\n" +
+                "INSERT INTO steam_item_sale.buff_item_prices\n" +
+                "    SELECT * FROM to_be_upserted\n" +
+                "    WHERE id NOT IN (SELECT id FROM updated);";
+
+        DecimalFormat df = new DecimalFormat("0.00");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+
+        List<String[]> result = new ArrayList<>();
 
         String url = "https://buff.163.com/market/goods?goods_id=" + id;
 
@@ -108,28 +202,65 @@ public class BuffCrawler {
         HtmlPage page = webClient.getPage(url);
         Thread.sleep(1000);
 
+        String hash_name = null;
+        List<DomElement> names = page.getByXPath("//*[contains(@class, 'cru-goods')]");
+        for (DomElement name : names) {
+            Document name_xml = new SAXReader().read(new StringReader(name.asXml()));
+            if (name_xml.valueOf("span") == null) {
+                continue;
+            }
+            hash_name = name_xml.valueOf("span").trim();
+        }
+
         List<com.gargoylesoftware.htmlunit.html.DomElement> Items = page.getByXPath("//*[contains(@class, 'relative-goods')]");
 
-        for (DomNode element : Items.get(0).getChildNodes()){
-            //System.out.println(element.asXml().trim());
-            String goodsId = null;
+        try (PreparedStatement pstmt = conn.prepareStatement(SQLUpsert, Statement.RETURN_GENERATED_KEYS)) {
 
-            String item_xml =  element.asXml().trim();
+            for (DomNode element : Items.get(0).getChildNodes()) {
+                //System.out.println(element.asXml().trim());
+                String goodsId;
 
-            Document document = null;
-            try {
-                document = new SAXReader().read(new StringReader(item_xml));
-                goodsId = document.valueOf("a/data-goods-id");
-                String price_rmb = document.valueOf("/a/.").replaceAll("\n", "").replaceAll("\t", "").replaceAll(" ", "").split("¥")[1];
+                org.w3c.dom.NamedNodeMap map = element.getAttributes();
 
-                //goodsId = goodsId!=null&&!"".equals(goodsId)?goodsId:""+id;
+                if (map.getLength() == 0) {
+                    continue;
+                }
 
-                System.out.println(goodsId);
-                System.out.println(price_rmb);
-            } catch (DocumentException e){
-                //Do nothing
+                try {
+                    goodsId = map.getNamedItem("data-goodsid").getNodeValue();
+                } catch (NullPointerException e) {
+                    goodsId = "" + id;
+                }
+
+                String price_rmb = element.asNormalizedText().replaceAll(" ", "").split("¥")[1];
+
+                System.out.print(goodsId + " ");
+                System.out.print(price_rmb + " ");
+
+                double price_euro = Double.parseDouble(df.format(conversionFromRMBtoEUR * Double.parseDouble(price_rmb)).replace(",", "."));
+                System.out.print(price_euro + " ");
+                System.out.println();
+
+                //id,price_euro,timestamp,success,name
+                pstmt.setInt(1, Integer.parseInt(goodsId));
+                pstmt.setDouble(2, price_euro);
+                pstmt.setBoolean(3, true);
+                if ((Integer.parseInt(goodsId)) == id) //name will only be inserted if there is no entry at all
+                {
+                    pstmt.setString(4, hash_name);
+                } else {
+                    pstmt.setString(4, "");
+                }
+
+                pstmt.addBatch();
+
             }
+            int[] updateCounts = pstmt.executeBatch();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
+        conn.commit();
     }
 }
 
--- src/main/resources/ddls/table_buff_item_price.sql
+++ src/main/resources/ddls/table_buff_item_price.sql
@@ -8,7 +8,7 @@ CREATE TABLE steam_item_sale.buff_item_prices (
 	price_euro float8 NULL,
 	id int4 NOT NULL,
 	"timestamp" timestamp NULL,
-	success bool NULL,
+	has_enterior bool NULL,
 	"name" varchar(999) NULL,
 	weapon_name varchar(999) NULL,
 	CONSTRAINT buff_item_prices_pk PRIMARY KEY (id)
