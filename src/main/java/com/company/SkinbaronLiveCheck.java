a86d1bb853c6d0f2ef445fedfc9f24437fd5a17f
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2021-10-17 11:07:46 +0200

    redesigned code

--- src/main/java/com/company/SkinbaronLiveCheck.java
+++ src/main/java/com/company/SkinbaronLiveCheck.java
@@ -1,10 +1,10 @@
 package com.company;
 
+import org.postgresql.util.PSQLException;
+
 import java.io.FileNotFoundException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import static com.company.SkinbaronAPI.getBalance;
 import static com.company.common.getConnection;
@@ -12,25 +12,25 @@ import static com.company.common.readPasswordFromFile;
 
 @SuppressWarnings("BusyWait")
 public class SkinbaronLiveCheck {
-    private static Connection conn;
 
-    public static void main(String[] args) throws FileNotFoundException, SQLException {
+    public static void main(String[] args) throws Exception {
 
         int wait_counter = 3;
 
         String secret = readPasswordFromFile("C:/passwords/api_secret.txt");
 
         while (true) {
-            try {
-                Connection conn = getConnection();
+            try (Connection conn = getConnection()){
 
                 System.out.println("Waiting for " + Math.pow(2, wait_counter) + " seconds");
                 Thread.sleep((long) (Math.pow(2, wait_counter) * 1000));
                 getBalance(secret,false,conn);
-                conn.close();
+                break;
+            } catch (PSQLException e) {
+                System.out.println("Postgres is down.");
                 break;
             } catch (Exception e) {
-                System.out.println("Kaputt!!!!");
+                System.out.println("Skibaron APIs are still down.");
                 wait_counter++;
 
             }
