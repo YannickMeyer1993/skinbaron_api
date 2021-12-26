34f5facfc8e98d41b06cce88ded22e423fe07804
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2021-12-26 20:09:32 +0100

    changed parameter

--- src/main/resources/ddls/view_auf_skinbaron_kaufbare_skins.sql
+++ src/main/resources/ddls/view_auf_skinbaron_kaufbare_skins.sql
@@ -12,6 +12,6 @@ SELECT smsr.name,
    FROM steam_item_sale.skinbaron_market_search_results smsr
      JOIN steam_item_sale.steam_most_recent_prices smrp ON smsr.name::text = smrp.name::text AND smrp.price_euro <> 0::double precision AND smsr.name::text !~~ '1Sticker |%'::text AND smsr.name::text !~~ 'Souvenir%'::text AND smsr.name::text !~~ 'Sealed Graffiti%'::text AND smrp.quantity > 50
      JOIN steam_item_sale.durchschnittspreise_steam s ON smsr.name::text = s.name::text AND s.dsratio < 1.5::double precision
-  WHERE ((smrp.price_euro - 0.04::double precision) / smsr.price::double precision) >= 1.6::double precision
+  WHERE ((smrp.price_euro - 0.04::double precision) / smsr.price::double precision) >= 1.75::double precision
   GROUP BY smsr.name, smsr.price, smrp.price_euro, smrp.quantity, smrp."timestamp", ((smrp.price_euro - 0.04::double precision) / smsr.price::double precision), s.dsratio
   ORDER BY ((smrp.price_euro - 0.04::double precision) / smsr.price::double precision) DESC;
