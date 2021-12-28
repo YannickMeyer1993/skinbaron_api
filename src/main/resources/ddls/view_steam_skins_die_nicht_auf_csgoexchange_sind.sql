1b8a71d08018a9346fbf3bc44718fda669ae5768
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2021-12-28 13:33:38 +0100

    deleted views

--- src/main/resources/ddls/view_steam_skins_die_nicht_auf_csgoexchange_sind.sql
+++
@@ -1,7 +0,0 @@
--- steam_item_sale.steam_skins_die_nicht_auf_csgoexchange_sind source
-
-CREATE OR REPLACE VIEW steam_item_sale.steam_skins_die_nicht_auf_csgoexchange_sind
-AS SELECT DISTINCT sip.name
-   FROM steam_item_sale.steam_item_prices sip
-  WHERE NOT (sip.name::text IN ( SELECT item_informations.name
-           FROM steam_item_sale.item_informations));
