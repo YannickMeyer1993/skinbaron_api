5e39cf9646c139b730d1dc014431889379566d6c
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2021-12-28 13:23:29 +0100

    deleted steam_max_prices

--- src/main/resources/ddls/view_steam_max_prices.sql
+++
@@ -1,9 +0,0 @@
-create or replace view steam_item_sale.steam_max_prices as
-with max_prices as
-(
-	select distinct steam."name",max(price_euro) as price_euro from steam_item_sale.steam_item_prices steam
-	group by "name" 
-)
-select distinct steam."name",max(steam.quantity) as max_quantity,max(steam."date") as date,max_prices.price_euro from steam_item_sale.steam_item_prices steam
-inner join max_prices on max_prices.name = steam ."name" and max_prices.price_euro = steam .price_euro
-group by steam ."name" ,max_prices.price_euro
