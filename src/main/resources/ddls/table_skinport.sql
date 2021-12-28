e6b956ece4e891b8e9dab94760018364a72a14ca
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2021-12-28 13:17:46 +0100

    deleted skinport table

--- src/main/resources/ddls/table_skinport.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE if not exists steam_item_sale."skinport" (
-  "market_hash_name" varchar(100) DEFAULT NULL,
-  "min_price" double precision DEFAULT NULL,
-  "quantity" int DEFAULT NULL,
-  "created_at" int DEFAULT NULL,
-  "updated_at" int DEFAULT NULL
-) ;
