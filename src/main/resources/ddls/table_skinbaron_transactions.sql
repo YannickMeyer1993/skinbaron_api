86b5a35117101a479259890af8f92f9f2ed641c8
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2021-12-28 13:23:01 +0100

    deleted skinbaron_transactions

--- src/main/resources/ddls/table_skinbaron_transactions.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE if not exists steam_item_sale.skinbaron_transactions (
-	"name" varchar(100) NULL DEFAULT NULL::character varying,
-	saleid varchar(100) NULL DEFAULT NULL::character varying,
-	"timestamp" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-	success bool NULL,
-	price float8 NULL
-);
--- src/main/resources/ddls/view_overview_bought_items_per_day.sql
+++
@@ -1,9 +0,0 @@
--- steam_item_sale.overview_bought_items_per_day source
-
-CREATE OR REPLACE VIEW steam_item_sale.overview_bought_items_per_day
-AS SELECT count(*) AS anzahl_skins,
-    sum(bs.price) AS summe,
-    date(bs."timestamp") AS datum
-   FROM steam_item_sale.skinbaron_transactions bs
-  WHERE bs.success
-  GROUP BY (date(bs."timestamp"));
