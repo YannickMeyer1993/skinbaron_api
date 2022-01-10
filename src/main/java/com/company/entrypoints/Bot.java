cda018b98dceddf88208f13575110a30e4c5f062
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2022-01-10 08:54:17 +0100

    commit

--- src/main/java/com/company/entrypoints/Bot.java
+++ src/main/java/com/company/entrypoints/Bot.java
@@ -43,140 +43,127 @@ public class Bot {
 
         Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
         logger.info("Buy items (true/false)?");
-        boolean buy_item =sc.nextBoolean();
+        boolean buy_item = sc.nextBoolean();
 
-        String secret = readPasswordFromFile("C:/passwords/api_secret.txt");
         double balance = getBalance();
 
         logger.info("Enter max price: ");
         max_price = min(sc.nextDouble(), balance);
 
-        String query = "select steam_preis_aktuell,skinbaron_preis,steam_preis, name from steam_item_sale.auf_skinbaron_kaufbare_skins where skinbaron_preis<=" + max_price + " order by rati desc";
-
-        //to get id
-        String query2 = "select s.name,s.id, s.price from steam_item_sale.skinbaron_market_search_results s where s.name = ? and  s.price <= ?";
+        String query = "select steam_price_is_new,skinbaron_price,steam_price, name,skinbaron_ids from steam.skinbaron_buyable_items where skinbaron_price<=" + max_price + " order by rati desc";
 
         while (true) {
             logger.info("Bot is started...");
             try (Connection conn = getConnection(); Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
 
                 while (rs.next()) {
-                    if (!rs.getBoolean("steam_preis_aktuell")) {
+                    if (!rs.getBoolean("steam_price_is_new")) {
                         double recent_price = getSteamPriceForGivenName(rs.getString("name"));
-                        if (recent_price < rs.getDouble("steam_preis")) {
-                            logger.info("Steam Preis nicht mehr aktuell für Item " + rs.getString("name") + ".");
+                        if (recent_price < rs.getDouble("steam_price")) {
+                            logger.info("steam price is now lower for item " + rs.getString("name") + ".");
                             continue;
                         }
                     }
 
-                    try (PreparedStatement pstmt = conn.prepareStatement(query2, Statement.RETURN_GENERATED_KEYS)) {
-                        pstmt.setString(1, rs.getString("name"));
-                        pstmt.setDouble(2, rs.getDouble("skinbaron_preis"));
+                    String skinbaron_ids = rs.getString("skinbaron_ids");
+                    String name = rs.getString("name");
+                    double price = rs.getDouble("price");
+                    double steam_price = rs.getDouble("steam_price");
 
-                        try (ResultSet rs2 = pstmt.executeQuery()) {
-
-                            String id = rs2.getString("id");
-                            String name = rs2.getString("name");
-                            double price = rs2.getDouble("price");
-                            double steam_price = rs.getDouble("steam_preis");
-
-                            while (rs2.next()) {
-                                logger.info(name + " " + id + " " + price);
-                                try {
-                                    if (buy_item) {
-                                        buyItem(id, price,steam_price);
-                                    } else {
-                                        boolean exists = checkIfExists(name, price);
-                                        if (!exists) {
-                                            deleteNonExistingSkinbaronItems(name,price);
-                                        }
-                                    }
-                                } catch (Exception e) {
-                                    logger.info("Item isn't there anymore.");
+                    for (String id : skinbaron_ids.split(",")) {
+                        logger.info(name + " " + id + " " + price);
+                        try {
+                            if (buy_item) {
+                                buyItem(id, price, steam_price);
+                            } else {
+                                boolean exists = checkIfExists(name, price);
+                                if (!exists) {
+                                    deleteNonExistingSkinbaronItems(name, price);
                                 }
                             }
+                        } catch (Exception e) {
+                            logger.info("Item isn't there anymore.");
                         }
                     }
                 }
-
-                logger.info("No Items found!.");
             }
+            logger.info("No Items found!.");
         }
     }
 
-    public static void buyItem( String itemId, Double price, double steamPrice) throws Exception {
+public static void buyItem(String itemId,Double price,double steamPrice)throws Exception{
 
-        String secret = readPasswordFromFile("C:/passwords/api_secret.txt");
-        String jsonInputString = "{\"apikey\": \"" + secret + "\",\"total\":" + price + ",\"saleids\":[\"" + itemId + "\"]}";
+        String secret=readPasswordFromFile("C:/passwords/api_secret.txt");
+        String jsonInputString="{\"apikey\": \""+secret+"\",\"total\":"+price+",\"saleids\":[\""+itemId+"\"]}";
 
-        HttpPost httpPost = new HttpPost("https://api.skinbaron.de/BuyItems");
-        httpPost.setHeader("Content.Type", "application/json");
-        httpPost.setHeader("x-requested-with", "XMLHttpRequest");
-        httpPost.setHeader("Accept", "application/json");
+        HttpPost httpPost=new HttpPost("https://api.skinbaron.de/BuyItems");
+        httpPost.setHeader("Content.Type","application/json");
+        httpPost.setHeader("x-requested-with","XMLHttpRequest");
+        httpPost.setHeader("Accept","application/json");
 
-        HttpClient client = HttpClients.custom()
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setCookieSpec(CookieSpecs.STANDARD).build())
-                .build();
+        HttpClient client= HttpClients.custom()
+        .setDefaultRequestConfig(RequestConfig.custom()
+        .setCookieSpec(CookieSpecs.STANDARD).build())
+        .build();
 
-        HttpEntity entity = new ByteArrayEntity(jsonInputString.getBytes(StandardCharsets.UTF_8));
+        HttpEntity entity=new ByteArrayEntity(jsonInputString.getBytes(StandardCharsets.UTF_8));
         httpPost.setEntity(entity);
-        HttpResponse response = client.execute(httpPost);
-        String result = EntityUtils.toString(response.getEntity());
+        HttpResponse response=client.execute(httpPost);
+        String result= EntityUtils.toString(response.getEntity());
 
         JSONObject resultJson;
-        try {
-            resultJson = (JSONObject) new JSONTokener(result).nextValue();
-        } catch (ClassCastException exp) {
-            logger.error(result);
-            throw new ClassCastException();
+        try{
+        resultJson=(JSONObject)new JSONTokener(result).nextValue();
+        }catch(ClassCastException exp){
+        logger.error(result);
+        throw new ClassCastException();
         }
 
-        if (resultJson.has("generalErrors")) {
-            String error = resultJson.get("generalErrors").toString();
-            logger.error(error);
-            if ("[\"some offer(s) are already sold\"]".equals(result) || "[\"count mismatch - maybe some offers have been sold or canceled or you provided wrong saleids\"]".equals(error)) {
-                requestDeleteSkinbaronId(itemId);
-            }
-            return;
+        if(resultJson.has("generalErrors")){
+        String error=resultJson.get("generalErrors").toString();
+        logger.error(error);
+        if("[\"some offer(s) are already sold\"]".equals(result)||"[\"count mismatch - maybe some offers have been sold or canceled or you provided wrong saleids\"]".equals(error)){
+        requestDeleteSkinbaronId(itemId);
+        }
+        return;
         }
 
-        JSONObject o = (JSONObject) resultJson.getJSONArray("items").get(0);
-        String name = o.getString("name");
-        price = o.getDouble("price");
+        JSONObject o=(JSONObject)resultJson.getJSONArray("items").get(0);
+        String name=o.getString("name");
+        price=o.getDouble("price");
 
         requestDeleteSkinbaronId(itemId);
-        logger.info("Item \"" + name + "\" was bought for " + price + ". Steam: " + steamPrice);
-    }
+        logger.info("Item \""+name+"\" was bought for "+price+". Steam: "+steamPrice);
+        }
 
-    public static void requestDeleteSkinbaronId(String id) {
-        String url = "http://localhost:8080/api/v1/DeleteSkinbaronId";
+public static void requestDeleteSkinbaronId(String id){
+        String url="http://localhost:8080/api/v1/DeleteSkinbaronId";
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
+        RestTemplate restTemplate=new RestTemplate();
+        HttpHeaders headers=new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        JSONObject JsonObject = new JSONObject();
+        JSONObject JsonObject=new JSONObject();
 
-        JsonObject.put("id", id);
+        JsonObject.put("id",id);
 
-        org.springframework.http.HttpEntity<String> request = new org.springframework.http.HttpEntity<>(JsonObject.toString(), headers);
+        org.springframework.http.HttpEntity<String> request=new org.springframework.http.HttpEntity<>(JsonObject.toString(),headers);
 
-        restTemplate.postForObject(url, request, String.class);
-    }
+        restTemplate.postForObject(url,request,String.class);
+        }
 
-    public static void deleteNonExistingSkinbaronItems(String ItemName,double price) {
-        String url = "http://localhost:8080/api/v1/DeleteNonExistingSkinbaronItems";
+public static void deleteNonExistingSkinbaronItems(String ItemName,double price){
+        String url="http://localhost:8080/api/v1/DeleteNonExistingSkinbaronItems";
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
+        RestTemplate restTemplate=new RestTemplate();
+        HttpHeaders headers=new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        JSONObject JsonObject = new JSONObject();
+        JSONObject JsonObject=new JSONObject();
 
-        JsonObject.put("ItemName", ItemName);
-        JsonObject.put("price", price);
+        JsonObject.put("ItemName",ItemName);
+        JsonObject.put("price",price);
 
-        org.springframework.http.HttpEntity<String> request = new org.springframework.http.HttpEntity<>(JsonObject.toString(), headers);
+        org.springframework.http.HttpEntity<String> request=new org.springframework.http.HttpEntity<>(JsonObject.toString(),headers);
 
-        restTemplate.postForObject(url, request, String.class);
-    }
-}
+        restTemplate.postForObject(url,request,String.class);
+        }
+        }
--- src/main/resources/PostgresDAO/init/4_view_skinbaron_buyable_items.sql
+++ src/main/resources/PostgresDAO/init/4_view_skinbaron_buyable_items.sql
@@ -1,12 +1,13 @@
 CREATE OR REPLACE VIEW steam.skinbaron_buyable_items AS
 SELECT skinbaronitems.name,
     (smrp.price_euro - 0.04::double precision) / skinbaronitems.price::double precision AS rati,
-    skinbaronitems.price AS skinbaron_preis,
-    smrp.price_euro AS steam_preis,
-    smrp.quantity AS anzahl_skins_auf_steam,
+    skinbaronitems.price AS skinbaron_price,
+    smrp.price_euro AS steam_price,
+    smrp.quantity AS steam_quantity,
     smrp."date" AS steam_date,
-    s.dsratio,
-    ROUND(cast(EXTRACT(EPOCH FROM (now() - smrp."date"))/(60*60*24)as numeric),1) <= 1 as steam_preis_aktuell
+    s.dsratio as steam_avg_ratio,
+    ROUND(cast(EXTRACT(EPOCH FROM (now() - smrp."date"))/(60*60*24)as numeric),1) <= 1 as steam_price_is_new,
+    string_agg(skinbaronitems.id,',') as skinbaron_ids
    FROM steam.skinbaron_items skinbaronitems
      JOIN steam.steam_current_prices smrp ON skinbaronitems.name::text = smrp.name::text AND smrp.price_euro <> 0::double precision AND skinbaronitems.name::text !~~ '1Sticker |%'::text AND skinbaronitems.name::text !~~ 'Souvenir%'::text AND skinbaronitems.name::text !~~ 'Sealed Graffiti%'::text AND smrp.quantity > 50
      JOIN steam.steam_avg_prices s ON skinbaronitems.name::text = s.name::text AND s.dsratio < 1.5::double precision
