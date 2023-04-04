9761d417508c0e847bcc8fc56778de2ffb605bbc
ymeyer <40489262+YannickMeyer1993@users.noreply.github.com>
2023-04-04 08:55:20 +0200

    changed parameters

--- src/main/resources/PostgresDAO/init/4_view_skinbaron_buyable_items.sql
+++ src/main/resources/PostgresDAO/init/4_view_skinbaron_buyable_items.sql
@@ -14,7 +14,7 @@ SELECT skinbaronitems.name,
     bcp.id as buff_id
 FROM steam.skinbaron_items skinbaronitems
 JOIN steam.steam_current_prices smrp ON skinbaronitems.name::text = smrp.name::text AND smrp.price_euro <> 0::double precision AND skinbaronitems.name::text !~~ 'Souvenir%'::text AND skinbaronitems.name::text !~~ 'Sealed Graffiti%'::text AND smrp.quantity > 50
-and ((smrp.price_euro - 0.04::double precision) / skinbaronitems.price::double precision) >= 1.85::double precision
+and ((smrp.price_euro - 0.04::double precision) / skinbaronitems.price::double precision) >= 1.8::double precision
 JOIN steam.steam_avg_prices s ON skinbaronitems.name::text = s.name::text AND s.dsratio < 1.5::double precision
 left join steam.buff_current_prices bcp on skinbaronitems."name" = bcp."name"
 GROUP BY skinbaronitems.name, skinbaronitems.price, smrp.price_euro, smrp.quantity, smrp."date", ((smrp.price_euro - 0.04::double precision) / skinbaronitems.price::double precision), s.dsratio,bcp.price_euro,bcp.insert_timestamp,bcp.id,bcp.has_exterior
